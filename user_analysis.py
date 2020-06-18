7d75c073ca5431e52acfe15aa9ac3eebd79b5ce1
Inbar Shirizly <in.shirizly@gmail.com>
2020-06-18 15:54:15 +0300

    add comment for the generator function

--- user_analysis.py
+++ user_analysis.py
@@ -35,14 +35,14 @@ class UserAnalysis(Website):
 
     def generate_users_links(self):
         """
-        generator which return url link for each individual user
+        generator which yields url link for each individual user
         1. the generator prints which website and page is been scrapping (for controlling the process, especially in
         the case of threading)
         2. If it's the first page to scrap, the value of the variable: first_instance_in_page will the first index needed
             (to prevent requesting unnecessary pages), else: it starts from the first user link
         3. for loop over the relevant users - retrieving the relevant link per individual user
 
-        :return:
+        :yield: url link for each individual user (str)
         """
 
         for i, soup in enumerate(self.get_pages_soups(self.get_first_url())):
