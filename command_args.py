9e74c004961dd9af795380dda73225047759000e
nir barazida <nirbarazida@gmail.com>
2020-07-05 17:14:51 +0300

    changes in defaults

--- command_args.py
+++ command_args.py
@@ -21,7 +21,7 @@ parser.add_argument('user_name', help="mysql user name", type=str)
 
 parser.add_argument('password', help="mysql user password", type=str)
 
-parser.add_argument('DB_name', help="databse name", type=str)
+parser.add_argument('--DB_name', help="databse name", type=str, default='SEDB')
 
 parser.add_argument('--num_users', help="Number of users to scrap", type=int, default=2)
 
@@ -36,14 +36,14 @@ parser.add_argument('--chunk_of_data', help="How many users to store in memory b
 
 parser.add_argument('--sleep_factor', help="Sleep factor between requests, default=1.5", default=1.5, type=float)
 
-parser.add_argument("--creat_DB", help="needs to create new DB? default=False "
-                    , type=bool_converter, default=False)
+parser.add_argument("--creat_DB", help="needs to create new DB? default=True "
+                    , type=bool_converter, default=True)
 
 parser.add_argument("--auto_scrap", help="start scraping from the last instance default=True "
                     , type=bool_converter, default=True)
 
 parser.add_argument("--multi_process", help="To use Multi Process or basic for loop between the different websites, "
-                                            "default=True "
-                    , type=bool_converter, default=True)
+                                            "default=False "
+                    , type=bool_converter, default=False)
 
 args = parser.parse_args()
